91ac0ca66f988ac7f2bccd1a44d58f09eba53bc4
Sylka <garcia.sylka343@gmail.com>
2022-05-19 16:35:02 -0500

    Actualizacion

--- GarciaSylka/Actividad-B2-C2/PuntoVenta.cpp
+++ GarciaSylka/Actividad-B2-C2/PuntoVenta.cpp
@@ -1,26 +1,36 @@
+//=======Nombre de programa:Punto de Ventas=======//
+//=======Archivo:GarciaSylka
+//=======Autor:Garcia Sylka
+//=======Feha de elaboracion:09-05-2022
+//=======Fecha de ultima actualizacion:19-05-2022
 #include<iostream>
 using namespace std;
 int main ()
 {
-	float x,c,a,r,vb1,vd,vb2,viva,fi,vt;
-	c=0;
-	a=0;
-	cout<<"\r\rPunto de Venta\r\r"<<endl;
+	float GS_x,GS_c,GS_a,GS_n,GS_vb1,GS_vd,GS_vb2,GS_viva,GS_fi,GS_vt;
+	GS_c=0;
+	GS_a=0;
+	cout<<"\n\nPunto de Venta\n\n"<<endl;
 	cout<<"Ingrese la cantidad de los productos que desea vender:";
-	cin>>r;
+	cin>>GS_n;
 	do{
 		cout<<"Ingrese el valor del producto:";
-		cin>>x;
-		c=c+1;
-		a=a+x;
-	} while(c<r);
-	 vb1=a;
-	 vd=vb1*0.10;
-	 viva=vb2*0.12;
-	 fi=vb2+viva;
-	 vt=fi-vd;
-	 cout<<"\r\rResultado\r\r"<<endl;
-	 cout<<"Valor con iva es:"<<fi<<endl;
-	 cout<<"Valor total es:"<<vt<<endl;
+		cin>>GS_x;
+		GS_c=GS_c+1;
+		GS_a=GS_a+GS_x;
+	} while(GS_c<GS_n);
+cout<<"//=======Nombre de programa:Punto de ventas=======//"<<endl;
+cout<<"//=======Archivo:GarciaSylka"<<endl;
+cout<<"//=======Autor:Garcia Sylka"<<endl;
+cout<<"//=======Fecha de elaboracion:09-05-2022"<<endl;
+cout<<"//=======Fecha de ultima actualizacion:19-05-2022"<<endl;
+	 GS_vb1=GS_a;
+	 GS_vd=GS_vb1*0.10;
+	 GS_viva=GS_vb2*0.12;
+	 GS_fi=GS_vb2+GS_viva;
+	 GS_vt=GS_fi-GS_vd;
+	 cout<<"\n\nResultado\n\n"<<endl;
+	 cout<<"Valor con iva es:"<<GS_fi<<endl;
+	 cout<<"Valor total es:"<<GS_vt<<endl;
 	 return 0;
 } 
