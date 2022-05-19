7bd6abc06c46dea80ea0aad566b2f2c94ed1283f
Sylka <garcia.sylka343@gmail.com>
2022-05-19 14:42:59 -0500

    Actualizacion

---
+++ GarciaSylka/Actividad-B2-C2/compara.cpp
@@ -0,0 +1,30 @@
+//=======Nombre de programa: Comparar dos numeros=======//
+//======Archivo:GarciaSylka-compara.cpp
+//======Autor:Garcia Sylka
+//======Fecha de elaboracion:07-05-2022
+//======Fecha de ultima actualizacion:19-05-2022
+#include <iostream>
+using namespace std;
+int main ()
+{
+
+	int GS_X,GS_Y;
+	cout<<"Ingrese los numeros a comparar"<<endl;
+	cin>>GS_X;
+	cin>>GS_Y;
+	if (GS_X==GS_Y)
+	cout<<"Son iguales"<<endl;
+	else
+
+cout<<"//=======Nombre de programa: Comparar dos numeros=======//"<<endl;
+cout<<"//=======Archivo:GarciaSylka-compara.cpp"<<endl;
+cout<<"//====≈==Autor:Garcia Sylka"<<endl;
+cout<<"//=======Fecha de elaboracion:07-05-2022"<<endl;
+cout<<"//=======Fecha de ultima elaboracion:19-05-2022"<<endl;
+		if (GS_X<GS_Y)
+			cout<<GS_X<<"Es menor"<<endl;
+		else
+			cout<<GS_Y<<"Es menor"<<endl;
+	return 0;
+} 
+
