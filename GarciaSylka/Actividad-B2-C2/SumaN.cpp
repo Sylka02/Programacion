cd867d5f41b02c1e2383da1cf461c31b73055c37
Sylka <garcia.sylka343@gmail.com>
2022-05-19 15:55:05 -0500

    Actualizacion

--- GarciaSylka/Actividad-B2-C2/SumaN.cpp
+++ GarciaSylka/Actividad-B2-C2/SumaN.cpp
@@ -1,18 +1,29 @@
+//=======Nombre del Programa:Suma de dos numeros=======//
+//=======Archivo:GarciaSylka-SumaN.cpp
+//=======Autor: Garcia Sylka
+//=======Fecha de elaboracion:07-05-2022
+//=======Fecha de ultima actualizacion:19-05-2022
 #include<iostream>
 using namespace std;
 int main ()
 {
-	int c=0,n;
-	float s=0,x;
+	int GS_c=0,GS_n;
+	float GS_s=0,GS_x;
 	cout<<"Ingrese la cantidad de valores a sumar:";
-	cin>>n;
+	cin>>GS_n;
 	do{
-		cout<<"Ingrese el elemento"<<c+1;
-		cin>>x;
-		c=c+1;
-		s=s+x;
-	}while(c<n);
-	cout<<"La suma de los elementos fue:"<<s<<endl;
+		cout<<"Ingrese el elemento"<<GS_c+1;
+		cin>>GS_x;
+		GS_c=GS_c+1;
+		GS_s=GS_s+GS_x;
+	}while(GS_c<GS_n);
+
+cout<<"//=======Nombre del programa:Suma de dos numeros=======//"<<endl;
+cout<<"Archivo:GarciaSylka-Suma.cpp"<<endl;
+cout<<"Autor: Garcia Sylka"<<endl;
+cout<<"Fecha de elaboracion:07-05-2022"<<endl;
+cout<<"Fecha de ultima actualizacion:19-05-2022"<<endl;
+	cout<<"La suma de los elementos fue:"<<GS_s<<endl;
 	return 0;
 }
 
