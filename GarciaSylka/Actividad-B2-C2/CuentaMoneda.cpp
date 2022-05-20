115e552fab690da4682a8a52fd249710c0a96341
Sylka <garcia.sylka343@gmail.com>
2022-05-20 10:21:14 -0500

    Actualizacion

--- GarciaSylka/Actividad-B2-C2/CuentaMoneda.cpp
+++ GarciaSylka/Actividad-B2-C2/CuentaMoneda.cpp
@@ -1,36 +1,46 @@
+//=======Nombre de programa: Cuentas de monedas=======//
+//=======Archivo:GarciaSylka
+//=======Autor: Garcia Sylka
+//=======Fecha de elaboracion:07-05-2022
+//=======Fecha de ultima actualizacion:19-05-2022
 #include<iostream>
 using namespace std;
 int main ()
 {
-	int n,c=0,c1=0,c2=0;
-	float x,a=0,a1=0,a2=0,m=0.10;
+	int GS_n,GS_c=0,GS_c1=0,GS_c2=0;
+	float GS_x,GS_a=0,GS_a1=0,GS_a2=0,GS_m=0.10;
 	cout<<"Cantidad de moneda a ingresar:";
-	cin>>n;
+	cin>>GS_n;
 	do
 	{
-	cout<<"Ingresar el valor de la moneda(0,10,0,25):";
-	cin>>x;
-	c=c+1;
-	a=a+x;
-	if (x==m)
+	cout<<"Ingresar el valor de la moneda(0.10,0.25):";
+	cin>>GS_x;
+	GS_c=GS_c+1;
+	GS_a=GS_a+GS_x;
+	if (GS_x==GS_m)
 	{
-		c1=c1+1;
-		a1=a1+x;
+		GS_c1=GS_c1+1;
+		GS_a1=GS_a1+GS_x;
 	}
 	else
 	{
-		c2=c2+1;
-		a2=a2+x;
+		GS_c2=GS_c2+1;
+		GS_a2=GS_a2+GS_x;
 	}
 	}
-	while (c<n);
+	while (GS_c<GS_n);
+cout<<"//=======Nombre de programa: Cuentas de monedas=======//"<<endl;
+cout<<"//=======Archivo:GarciaSylka"<<endl;
+cout<<"//=======Autor: Garcia Sylka"<<endl;
+cout<<"//=======Fecha de elaboracion:07-05-2022"<<endl;
+cout<<"//=======Fecha de ultima actualizacion:19-05-2022"<<endl;
 	cout<<"El resultado fue:"<<endl;
-	cout<<"Cantidad de monedas ingresadas:"<<c<<endl;
-	cout<<"Cantidad total en dinero contado:"<<a<<endl;
-	cout<<"Cantidad de moneda de 0.10c ingresadas:"<<c1<<endl;
-	cout<<"Cantidad total en dinero de monedas de 0.10cc:"<<a1<<endl;
-	cout<<"Cantidad de moneda de 0.25cc ingresada:"<<c2<<endl;
-	cout<<"Cantidad total en dinero de moneda de 0.25cc:"<<a2<<endl;
+	cout<<"Cantidad de monedas ingresadas:"<<GS_c<<endl;
+	cout<<"Cantidad total en dinero contado:"<<GS_a<<endl;
+	cout<<"Cantidad de moneda de 0.10c ingresadas:"<<GS_c1<<endl;
+	cout<<"Cantidad total en dinero de monedas de 0.10cc:"<<GS_a1<<endl;
+	cout<<"Cantidad de moneda de 0.25cc ingresada:"<<GS_c2<<endl;
+	cout<<"Cantidad total en dinero de moneda de 0.25cc:"<<GS_a2<<endl;
 	
 	return 0;
 }
